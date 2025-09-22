16316c77d48c2b769de763a5a8cbfdedbb045f9c
epicmann21 <kiddos505098@gmail.com>
2025-09-22 15:20:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
+// index.js
 const express = require("express");
 const { Server } = require("colyseus");
 const { WebSocketTransport } = require("@colyseus/ws-transport");
 const { createServer } = require("http");
+const { Schema, MapSchema, type } = require("@colyseus/schema");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 const server = createServer(app);
 
-// Colyseus setup
-const gameServer = new Server({
-  transport: new WebSocketTransport({ server })
-});
+// Player schema
+class Player extends Schema {
+  @type("number") x = 0;
+  @type("number") y = 0;
+  @type("number") z = 0;
+}
 
-// Basic room
+// State schema
+class State extends Schema {
+  @type({ map: Player }) players = new MapSchema();
+}
+
+// Game room
 const { Room } = require("colyseus");
 
 class GameRoom extends Room {
-  onCreate(options) {
-    console.log("Room created!");
+  onCreate() {
+    this.setState(new State());
+
+    this.onMessage("move", (client, data) => {
+      const player = this.state.players[client.sessionId];
+      if (player) {
+        player.x = data.x;
+        player.y = data.y;
+        player.z = data.z;
+      }
+    });
   }
 
   onJoin(client) {
-    console.log(client.sessionId, "joined!");
-  }
-
-  onMessage(client, message) {
-    console.log(client.sessionId, "sent:", message);
+    const player = new Player();
+    this.state.players[client.sessionId] = player;
+    console.log(client.sessionId, "joined");
   }
 
   onLeave(client) {
-    console.log(client.sessionId, "left!");
+    delete this.state.players[client.sessionId];
+    console.log(client.sessionId, "left");
   }
 }
+
+const gameServer = new Server({
+  transport: new WebSocketTransport({ server })
+});
 
 gameServer.define("game_room", GameRoom);
 
